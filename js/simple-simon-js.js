87752bd0f3fc1db6869f72f9c77ed0bb5118fd08
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-12 16:04:14 -0500

    re-ordered colors

--- js/simple-simon-js.js
+++ js/simple-simon-js.js
@@ -3,30 +3,37 @@
 var gameSequence = [];
 
 function getRandomColor() {
-    var randomNumber = Math.floor((Math.random() * 1) + 1);
+    var randomNumber = Math.floor((Math.random() * 4) + 1);
     return randomNumber;
 }
 
-$('#start-game').click(function(event) {
-        var colorCode = getRandomColor();
-        console.log(colorCode);
-        if(colorCode === 1) {
-            $('#red').animate({opacity: "1"});
-            gameSequence.push(colorCode);
-        }
-        if(colorCode === 2) {
-            $('#blue').css('opacity', '1');
-            gameSequence.push(colorCode);
-        }
-        if(colorCode === 3) {
-            $('#green').css('opacity', '1');
-            gameSequence.push(colorCode);
-        }
-        if(colorCode === 4) {
-            $('#yellow').css('opacity', '1');
-            gameSequence.push(colorCode);
-        }
+function gamePlay() {
+    var colorCode = getRandomColor();
+    console.log(colorCode);
+    if(colorCode === 1) {
+        $('#green').animate({opacity: "1"});
+        gameSequence.push(colorCode);
+    }
+    if(colorCode === 2) {
+        $('#red').css('opacity', '1');
+        gameSequence.push(colorCode);
+    }
+    if(colorCode === 3) {
+        $('#yellow').css('opacity', '1');
+        gameSequence.push(colorCode);
+    }
+    if(colorCode === 4) {
+        $('#blue').css('opacity', '1');
+        gameSequence.push(colorCode);
+    }
     console.log(gameSequence);
+}
+
+$('#start-game').click(function(event){
+    gamePlay();
+    // $(this).css("display", "none");
+
 });
+
 
 
