a00ae8db7e4ccf6423ef659af0a55d2442792e91
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-12 16:18:13 -0500

    added animation on click

--- js/simple-simon-js.js
+++ js/simple-simon-js.js
@@ -11,19 +11,27 @@ function gamePlay() {
     var colorCode = getRandomColor();
     console.log(colorCode);
     if(colorCode === 1) {
-        $('#green').animate({opacity: "1"});
+        $('#green').animate({opacity: "1"}, "fast", "linear", function(){
+            $(this).fadeTo("opacity", "0.5");
+        });
         gameSequence.push(colorCode);
     }
     if(colorCode === 2) {
-        $('#red').css('opacity', '1');
+        $('#red').animate({opacity: "1"}, "fast", "linear", function(){
+            $(this).fadeTo("opacity", "0.5");
+        });
         gameSequence.push(colorCode);
     }
     if(colorCode === 3) {
-        $('#yellow').css('opacity', '1');
+        $('#yellow').animate({opacity: "1"}, "fast", "linear", function(){
+            $(this).fadeTo("opacity", "0.5");
+        });
         gameSequence.push(colorCode);
     }
     if(colorCode === 4) {
-        $('#blue').css('opacity', '1');
+        $('#blue').animate({opacity: "1"}, "fast", "linear", function(){
+            $(this).fadeTo("opacity", "0.5");
+        });
         gameSequence.push(colorCode);
     }
     console.log(gameSequence);
