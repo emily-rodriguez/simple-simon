9c185eafe732875781ad5541e72b0622f64dcc7b
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-13 10:42:15 -0500

    changed if to switch statement, took

--- js/simple-simon-js.js
+++ js/simple-simon-js.js
@@ -2,46 +2,50 @@
 
 var gameSequence = [];
 
+var greenButton = $('#green');
+var redButton = $('#red');
+var yelButton = $('#yellow');
+var blueButton = $('#blue');
+
 function getRandomColor() {
     var randomNumber = Math.floor((Math.random() * 4) + 1);
     return randomNumber;
 }
 
+function animateColor(object) {
+    object.animate({opacity: "1"}, "fast", "linear", function(){
+        $(this).fadeTo("opacity", "0.5");
+        gameSequence.push(gameSequence);
+    };
+
 function gamePlay() {
     var colorCode = getRandomColor();
     console.log(colorCode);
-    if(colorCode === 1) {
-        $('#green').animate({opacity: "1"}, "fast", "linear", function(){
-            $(this).fadeTo("opacity", "0.5");
-        });
-        gameSequence.push(colorCode);
-    }
-    if(colorCode === 2) {
-        $('#red').animate({opacity: "1"}, "fast", "linear", function(){
-            $(this).fadeTo("opacity", "0.5");
-        });
-        gameSequence.push(colorCode);
-    }
-    if(colorCode === 3) {
-        $('#yellow').animate({opacity: "1"}, "fast", "linear", function(){
-            $(this).fadeTo("opacity", "0.5");
-        });
-        gameSequence.push(colorCode);
-    }
-    if(colorCode === 4) {
-        $('#blue').animate({opacity: "1"}, "fast", "linear", function(){
-            $(this).fadeTo("opacity", "0.5");
-        });
-        gameSequence.push(colorCode);
+    switch(colorCode) {
+        case 1:
+            animateColor(greenButton);
+            break;
+        case 2:
+            animateColor(redButton);
+            break;
+        case 3:
+            animateColor(yelButton);
+            break;
+        case 4:
+            animateColor(blueButton);
+            break;
+        default:
+            console.log("This should never happen");
+            break
     }
     console.log(gameSequence);
 }
 
 $('#start-game').click(function(event){
     gamePlay();
-    $(this).css("display", "none");
-    var i = 0;
-    console.log()
+    // $(this).css("display", "none");
+    // var i = 0;
+    // console.log()
 
 });
 
